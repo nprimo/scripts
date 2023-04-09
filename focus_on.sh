37dceb958a55f5dbdac2488cf2e29c9df09e41c2
nprimo <primo.niccolo@gmail.com>
2023-04-09 19:16:28 +0200

    fix(focus_on): update awk command

--- focus_on.sh
+++ focus_on.sh
@@ -3,7 +3,7 @@
 IFS='
 '
 
-APP_ID=$(wmctrl -l | awk -F"NF" '{print $NF}' | grep -E "$2" | awk '{print $1}')
+APP_ID=$(wmctrl -l | awk -v app=$2 'match($(NF-1)$NF, app) { print $1 }')
 if [ -n "$APP_ID" ]; then
     echo Focus "$APP_ID"
     wmctrl -ia "$APP_ID"
